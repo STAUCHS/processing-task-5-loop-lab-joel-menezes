2cc7fff3340c9de41553f6f6714e2cae33e1466e
joel-menezes <joel.menezes25@ycdsbk12.ca>
2024-03-25 13:51:44 -0400

    COMMITED

--- Sketch.java
+++ Sketch.java
@@ -3,7 +3,9 @@ import processing.core.PApplet;
 public class Sketch extends PApplet {
 
   /**
-   * Called once at the beginning of execution, put your size all in this method
+   * A program that uses nested loops and draws a bunch of squares
+   * 
+   * @author: Joel Menezes
    */
   public void settings() {
     // put your size call here
